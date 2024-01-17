8c7203ac14938bf921699dd5176fe2a892823cdb
Justin Kuntz <justin@justtech.dev>
2024-01-17 09:41:11 -0700

    updated redpanda entrypoint script

--- redpanda/entrypoint.sh
+++ redpanda/entrypoint.sh
@@ -1,30 +1,5 @@
 #! /usr/bin/env /bin/bash
 set -x
-echo "Starting local redpanda..."
-/usr/bin/rpk redpanda start \
---smp 1 \
---overprovisioned \
---kafka-addr internal://0.0.0.0:9092 \
---advertise-kafka-addr internal://redpanda:9092 \
---pandaproxy-addr internal://0.0.0.0:8082 \
---advertise-pandaproxy-addr internal://redpanda:8082 \
---schema-registry-addr internal://0.0.0.0:8081 \
---rpc-addr redpanda:33145 \
---advertise-rpc-addr redpanda:33145 \
---mode dev-container &
-
-echo "Finalizing topic configuration..."
-/usr/bin/rpk topic delete tpch packages transitions
-/usr/bin/rpk topic create \
-  -r 1 \
-  -p 16 \
-  -c cleanup.policy=delete \
-  -c  log_retention_ms=3600000 \
-  tpch packages transitions
-
-echo "Stopping local redpanda..."
-/usr/bin/rpk redpanda stop
-
 echo "Starting public redpanda..."
 /usr/bin/rpk redpanda start \
 --smp 1 \
@@ -37,3 +12,10 @@ echo "Starting public redpanda..."
 --rpc-addr redpanda:33145 \
 --advertise-rpc-addr redpanda:33145 \
 --mode dev-container
+
+echo "Finalizing topic configuration..."
+/usr/bin/rpk topic delete tpch packages transitions
+/usr/bin/rpk topic create -r 1 -p 16 -c cleanup.policy=delete -c  log_retention_ms=3600000 tpch packages transitions
+
+echo "Stopping local redpanda..."
+/usr/bin/rpk redpanda stop
